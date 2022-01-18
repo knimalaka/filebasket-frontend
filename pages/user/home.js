c28eef58438533253ef8ffeb566c525b8c6749cf
knimalaka <kavindanimalaka97@gmail.com>
2022-01-18 14:51:16 +0530

    user font page

--- pages/user/home.js
+++ pages/user/home.js
@@ -10,6 +10,15 @@ export default function Home() {//View, upload, signout options
   const SingleFileChange = (e) =>{
     setSingleFile(e.target.files[0]);
   }
+
+  const MultipleFileChange = (e) => {
+    setMultipleFiles(e.target.files);
+  }
+
+  const UploadMultipleFiles = async() => {
+    console.log(multipleFiles);
+  }
+
   return (
     <div className={styles.container}> 
       <main className={styles.main}>
@@ -46,12 +55,12 @@ export default function Home() {//View, upload, signout options
             <div className='col-6'>
             <div className='form-group'>
                 <label>Select Multiple Files</label>
-                <input type='file' className='form-control' multiple/>
+                <input type='file' onChange={(e) => MultipleFileChange(e)} className='form-control' multiple/>
               </div>
               </div>
               <div className='row'>
                 <div className='col-10'>
-                  <button type='button'>upload</button>
+                  <button type='button' onClick={() => UploadMultipleFiles()}>upload</button>
                 </div>
               </div>
           </div>
